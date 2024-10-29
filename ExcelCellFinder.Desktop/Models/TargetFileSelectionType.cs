c21e03a2b5bce44ca8131f0c0074408295a52cf2
大内勇人 <hayato_ouchi@yayoi-kk.co.jp>
2024-10-29 17:10:45 +0900

    個別ファイルを指定して実行できるように改善

---
+++ ExcelCellFinder.Desktop/Models/TargetFileSelectionType.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelCellFinder.Desktop.Models
+{
+    public enum TargetFileSelectionType
+    {
+        CSVファイルで指定,
+        フォルダを指定,
+        ファイルを指定
+    }
+}
--- ExcelCellFinder.Desktop/ViewModels/Pages/SetupConditionPageViewModel.cs
+++ ExcelCellFinder.Desktop/ViewModels/Pages/SetupConditionPageViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using ExcelCellFinder.Core.Options;
 using ExcelCellFinder.Core.Options.Interface;
+using ExcelCellFinder.Desktop.Models;
 using ExcelCellFinder.Desktop.Services;
 using ExcelCellFinder.Desktop.Services.CollectFileList;
 using ExcelCellFinder.Desktop.Services.FindCell;
@@ -23,6 +24,7 @@ namespace ExcelCellFinder.Desktop.ViewModels.Pages
 
         private static readonly string BUTTON_CONTENT_CSV = "CSVファイルを選択";
         private static readonly string BUTTON_CONTENT_DIRECTORY = "フォルダを選択";
+        private static readonly string BUTTON_CONTENT_SPECIFIC_FILE = "ファイルを選択";
 
         [ObservableProperty]
         private string _findFolderPath;
@@ -33,36 +35,79 @@ namespace ExcelCellFinder.Desktop.ViewModels.Pages
         [ObservableProperty]
         private string _excludeDirectoryRegex = "";
 
-        private bool _isUseFileListCsv = true;
-        public bool IsUseFileListCsv
-        {
-            get { return _isUseFileListCsv; }
-            set
-            {
-                SetProperty(ref _isUseFileListCsv, value);
-                IsProcessDirectory = !value;
-
-                OpenSelectPathButtonContent = value ? BUTTON_CONTENT_CSV : BUTTON_CONTENT_DIRECTORY;
-            }
-        }
+        [ObservableProperty]
+        private bool _isUseFileListCsv;
 
         [ObservableProperty]
         private bool _isProcessDirectory;
 
         [ObservableProperty]
+        private bool _isSpecificFile;
+
+        [ObservableProperty]
         private string _openSelectPathButtonContent = BUTTON_CONTENT_CSV;
+
+        public static IEnumerable<TargetFileSelectionType> TargetFileSelectionTypes => Enum.GetValues<TargetFileSelectionType>();
+
+        private TargetFileSelectionType _selectedTargetFileSelectionType = TargetFileSelectionType.CSVファイルで指定;
+        public TargetFileSelectionType SelectedTargetFileSelectionType
+        {
+            get { return _selectedTargetFileSelectionType; }
+            set
+            {
+                SetProperty(ref _selectedTargetFileSelectionType, value);
+
+                switch (value)
+                {
+                    case TargetFileSelectionType.CSVファイルで指定:
+                        IsUseFileListCsv = true;
+                        IsProcessDirectory = false;
+                        OpenSelectPathButtonContent = BUTTON_CONTENT_CSV;
+                        break;
+                    case TargetFileSelectionType.フォルダを指定:
+                        IsUseFileListCsv = false;
+                        IsProcessDirectory = true;
+                        OpenSelectPathButtonContent = BUTTON_CONTENT_DIRECTORY;
+                        break;
+                    case TargetFileSelectionType.ファイルを指定:
+                        IsUseFileListCsv = false;
+                        IsProcessDirectory = false;
+                        OpenSelectPathButtonContent = BUTTON_CONTENT_SPECIFIC_FILE;
+                        break;
+                }
+            }
+        }
 
         [RelayCommand]
         private void SelectPath()
         {
-            var dialog = new CommonOpenFileDialog(IsUseFileListCsv ? "ファイルを選択" : "フォルダを選択")
-            {
-                IsFolderPicker = IsUseFileListCsv == false
-            };
+            CommonOpenFileDialog dialog;
 
-            if (IsUseFileListCsv)
+            switch (SelectedTargetFileSelectionType)
             {
-                dialog.Filters.Add(new CommonFileDialogFilter("CSVファイル", "*.csv"));
+                case TargetFileSelectionType.CSVファイルで指定:
+                    dialog = new CommonOpenFileDialog("CSVファイルを選択")
+                    {
+                        Filters = { new CommonFileDialogFilter("CSVファイル", "*.csv") }
+                    };
+                    break;
+                case TargetFileSelectionType.フォルダを指定:
+                    dialog = new CommonOpenFileDialog("フォルダを選択")
+                    {
+                        IsFolderPicker = true
+                    };
+                    break;
+                case TargetFileSelectionType.ファイルを指定:
+                    dialog = new CommonOpenFileDialog("ファイルを選択")
+                    {
+                        Filters =
+                        {
+                            new CommonFileDialogFilter("EXCELファイル", "*.xlsx,*.xlsm")
+                        }
+                    };
+                    break;
+                default:
+                    return;
             }
 
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
@@ -100,17 +145,24 @@ namespace ExcelCellFinder.Desktop.ViewModels.Pages
         {
             var option = OptionFactory.GetOption();
 
-            if (IsUseFileListCsv)
+            switch (SelectedTargetFileSelectionType)
             {
-                option.TargetFileInfos = CollectFileListServiceFactory.GetService().CollectFileList(new FileInfo(FindFolderPath));
-                option.Mode = TargetMode.FileList;
-            }
-            else
-            {
-                option.TargetDirectoryInfo = new System.IO.DirectoryInfo(FindFolderPath);
-                option.Mode = TargetMode.Directory;
-                option.IsRecursively = IsRecursively;
-                option.ExcludeDirectoryRegex = string.IsNullOrEmpty(ExcludeDirectoryRegex) ? null : new Regex(ExcludeDirectoryRegex);
+                case TargetFileSelectionType.CSVファイルで指定:
+                    option.TargetFileInfos = CollectFileListServiceFactory.GetService().CollectFileList(new FileInfo(FindFolderPath));
+                    option.Mode = TargetMode.FileList;
+                    break;
+
+                case TargetFileSelectionType.フォルダを指定:
+                    option.TargetDirectoryInfo = new System.IO.DirectoryInfo(FindFolderPath);
+                    option.Mode = TargetMode.Directory;
+                    option.IsRecursively = IsRecursively;
+                    option.ExcludeDirectoryRegex = string.IsNullOrEmpty(ExcludeDirectoryRegex) ? null : new Regex(ExcludeDirectoryRegex);
+                    break;
+
+                case TargetFileSelectionType.ファイルを指定:
+                    option.TargetFileInfo = new System.IO.FileInfo(FindFolderPath);
+                    option.Mode = TargetMode.File;
+                    break;
             }
 
             option.TargetCellTypes = new[] { TargetCellType.RedColor, TargetCellType.StrikeLine };
